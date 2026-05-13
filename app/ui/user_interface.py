cf0a389669ac36ed1d0b2f0fed95ce815e3ec925
MLioulias <maiclaren@gmail.com>
2026-05-13 18:22:35 +0300

    Fix MainPage widget

--- app/ui/user_interface.py
+++ app/ui/user_interface.py
@@ -35,8 +35,9 @@ class WelcomeFrame(Frame):
     def __init__(self, parent, app):
         super().__init__(parent)
         self.app = app
+        
 
-        welcome_label = Label(self, text="Welcome!\n\nSign up to create an account or sign in to access an existing one.")
+        welcome_label = Label(self, text="Welcome to Wallet-App!\n\nSign up to create an account or sign in to access an existing one.")
         welcome_label.grid(row=0, column=0, columnspan=2, pady=10)
         
         access_buttons_frame = Frame(self)
@@ -54,27 +55,30 @@ class SignUpFrame(Frame):
         super().__init__(parent)
         self.app = app
 
+        signin_title = Label(self, text="Wallet App - Sign Up", font=("Arial",10))
+        signin_title.grid(row=0, column=0, columnspan=2, pady=10)
+
         label_username = Label(self, text= "Username")
-        label_username.grid(row=0, column=0, padx=5, pady=10)
+        label_username.grid(row=1, column=0, padx=5, pady=10)
 
         #Κάνουμε τα entry (usernmae & password) attributes για να μπορέσουμε να πάρουμε με .get() τα values
         self.entry_username = Entry(self)
-        self.entry_username.grid(row=0, column=1, padx=5, pady=10)
+        self.entry_username.grid(row=1, column=1, padx=5, pady=10)
 
         label_password = Label(self, text= "Password")
-        label_password.grid(row=1, column=0, padx=5, pady=5)
+        label_password.grid(row=2, column=0, padx=5, pady=5)
         
         self.entry_password = Entry(self)
-        self.entry_password.grid(row=1, column=1, padx=5, pady=5)
+        self.entry_password.grid(row=2, column=1, padx=5, pady=5)
 
         label_password_requirements = Label(self, text= "Use at least 2 characters (one Letter & one Number)")
-        label_password_requirements.grid(row=2, column=1,padx=5, pady=2)
+        label_password_requirements.grid(row=3, column=1,padx=5, pady=2)
 
         self.label_status = Label(self, text="", fg='red')
-        self.label_status.grid(row=4, column=0, columnspan=2, pady=5)
+        self.label_status.grid(row=5, column=0, columnspan=2, pady=5)
 
         control_signup_buttons_frame = Frame(self)
-        control_signup_buttons_frame.grid(row=2, column=2, padx=5, pady=5)
+        control_signup_buttons_frame.grid(row=3, column=2, padx=5, pady=5)
 
         button_save = Button(control_signup_buttons_frame, text="Create account", command = self.create_account)
         button_save.pack(side="left", padx=5)
@@ -101,30 +105,32 @@ class SignInFrame(Frame):
     def __init__(self,parent,app):
         super().__init__(parent)
         self.app = app
+        signin_title = Label(self, text="Wallet App - Sign In", font=("Arial",10))
+        signin_title.grid(row = 0, column=0, columnspan = 2, pady=5)
 
         self.label_signin_status = Label(self, text="", fg = 'red')
         self.label_signin_status.grid(row = 4, column=0, columnspan = 2, pady=5)
 
         label_username = Label(self, text= "Username")
-        label_username.grid(row=0, column=0, padx=5, pady=10)
+        label_username.grid(row=1, column=0, padx=5, pady=10)
 
         self.entry_username = Entry(self)
-        self.entry_username.grid(row=0, column=1, padx=5, pady=10)
+        self.entry_username.grid(row=1, column=1, padx=5, pady=10)
 
         label_password = Label(self, text= "Password")
-        label_password.grid(row=1, column=0, padx=5, pady=5)
+        label_password.grid(row=2, column=0, padx=5, pady=5)
         
         self.entry_password = Entry(self, show='*')
-        self.entry_password.grid(row=1, column=1, padx=5, pady=5)    
+        self.entry_password.grid(row=2, column=1, padx=5, pady=5)    
 
         control_signin_buttons_frame = Frame(self)
-        control_signin_buttons_frame.grid(row=2, column=0, columnspan=2, pady=10)
+        control_signin_buttons_frame.grid(row=3, column=0, columnspan=2, pady=10)
 
         button_signin = Button(control_signin_buttons_frame, text="Sign in", command=self.check_account)
-        button_signin.pack(side="left", padx=5)
+        button_signin.pack(side="left",padx=5)
 
         button_back = Button(control_signin_buttons_frame, text="Back", command = lambda: self.app.show_frame(WelcomeFrame))
-        button_back.pack(side="left", padx=5)
+        button_back.pack(side="left",padx=5)
 
     def check_account(self):
         username = self.entry_username.get().strip()
@@ -146,8 +152,26 @@ class MainPage(Frame):
     def __init__(self,parent,app):
         super().__init__(parent)
         self.app = app
+        self.app.root.title("Wallet App - Dashboard")
         self.app.set_menu_at_mainPage()
+        
+        mainframe_title = Label(self, text="Wallet App Dashboard", font=("Arial",18))
+        mainframe_title.pack(pady=20)
 
+        welcome_label = Label(self, text="You are logged in to your personal account.")
+        welcome_label.pack(pady=10)
+
+        new_entry_button = Button(self, text="New entry", command=lambda: self.app.show_frame(NewEntry))
+        new_entry_button.pack(pady=5)
+
+        inspect_button = Button(self, text="Inspect entries")
+        inspect_button.pack(pady=5)        
+        
+        stats_button = Button(self, text="Entries statistics")
+        stats_button.pack(pady=5)   
+        
+        logout_button = Button(self, text="Log out", command=lambda: self.app.show_frame(WelcomeFrame))
+        logout_button.pack(pady=20) 
 
 class NewEntry(Frame):
     def __init__(self,parent,app):
@@ -163,13 +187,22 @@ class NewEntry(Frame):
         self.entry_type.bind("<<ComboboxSelected>>", self.handle_entry_type)
         self.recurring_var = IntVar()
 
+        control_signup_buttons_frame = Frame(self)
+        control_signup_buttons_frame.grid(row=3, column=2, padx=5, pady=5)
+
+        #button_proceed = Button(control_signup_buttons_frame, text="Proceed", command=self.revenue_expense_view)
+        #button_proceed.pack(side="left", padx=5)
+
+        button_cancel = Button(control_signup_buttons_frame, text="Cancel", command = lambda: self.app.show_frame(MainPage))
+        button_cancel.pack(side="left", padx=5)
+
     def handle_entry_type(self, event):
         selected_type = self.entry_type.get()
 
         if selected_type == "Revenue" or selected_type == "Expense":
-            self.revenue_view()
+            self.revenue_expense_view()
 
-    def revenue_view(self):
+    def revenue_expense_view(self):
         
             date_label = Label(self, text="Date (dd/mm/yyyy)")
             date_label.grid(row=1,column=0,padx=5,pady=10)
